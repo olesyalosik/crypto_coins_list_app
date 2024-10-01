5b1d0be1b9f3ef3abd9e1d6af7a84f3217d650ff
olesyalosik <losiko2004@gmail.com>
2024-10-01 11:07:32 +0300

    add API support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:developer';
+import 'package:crypto_coins_list_app/crypto_coins_list_app.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,114 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CryptoCurrenciesList',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-        primaryColor: Colors.yellow,
-        scaffoldBackgroundColor: Colors.black12,
-        textTheme: const TextTheme(
-          bodyMedium: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w500,
-            fontSize: 20,
-          ),
-          labelSmall: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.w300,
-            fontSize: 15,
-          ),
-        ),
-        dividerColor: Colors.grey,
-        iconTheme: IconThemeData(
-          color: Colors.white,
-        ),
-        appBarTheme: AppBarTheme(
-          centerTitle: true,
-          backgroundColor: Colors.black12,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
-            fontWeight: FontWeight.w700,
-          ),
-        ),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-        title: Text(
-          'CryptoCurrenciesList',
-          style: Theme.of(context).appBarTheme.titleTextStyle,
-        ),
-      ),
-      body: ListView.separated(
-        separatorBuilder: (context, index) => Divider(
-          color: Theme.of(context).dividerColor,
-        ),
-        itemCount: 10,
-        itemBuilder: (context, i) => ListTile(
-          leading: SvgPicture.asset(
-            'assets/img/bitcoin.svg',
-            height: 25.0,
-            width: 25.0,
-          ),
-          title: Text(
-            'Bitcoin',
-            style: theme.textTheme.bodyMedium,
-          ),
-          subtitle: Text(
-            '200\$',
-            style: theme.textTheme.labelSmall,
-          ),
-          trailing: Icon(
-            Icons.arrow_forward_ios,
-            color: Theme.of(context).iconTheme.color,
-          ),
-        ),
-      ),
-    );
+    return cryptoCoinsListApp;
   }
 }
